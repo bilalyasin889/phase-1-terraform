b14001c5bab48c8aacb7a5a2f18f9e0de8cec34e
bilalyasin889 <56312400+bilalyasin889@users.noreply.github.com>
2026-04-06 15:24:01 +0100

    Provision CodeDeploy resources, including IAM roles, application, deployment group, and rollback configuration

---
+++ deployment.tf
@@ -0,0 +1,52 @@
+data "aws_iam_policy_document" "codedeploy_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codedeploy_service_role" {
+  name               = "${local.app_name}-codedeploy-service-role"
+  assume_role_policy = data.aws_iam_policy_document.codedeploy_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_role.codedeploy_service_role.name
+}
+
+resource "aws_codedeploy_app" "this" {
+  name = "${local.app_name}-deploy"
+}
+
+resource "aws_codedeploy_deployment_group" "this" {
+  app_name              = aws_codedeploy_app.this.name
+  deployment_group_name = "${local.environment}-main-group"
+  service_role_arn      = aws_iam_role.codedeploy_service_role.arn
+
+  autoscaling_groups = [module.asg.autoscaling_group_name]
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
+  }
+
+  load_balancer_info {
+    target_group_info {
+      name = module.alb.target_groups["app-tg"].name
+    }
+  }
+
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
+}
